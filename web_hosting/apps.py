812c9aa0dd3dfad009381758541ca5ebdd5e7db1
Khan Kim <Khancansoup@gmail.com>
2019-10-20 03:18:31 -0400

    first

--- web_hosting/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class WebHostingConfig(AppConfig):
-    name = 'web_hosting'
--- web_hosting/forms.py
+++
@@ -1,5 +0,0 @@
-from django import forms
-
-class RestaurantForm(forms.Form):
-   user_input = forms.CharField(label='Search for questions related to restaurant', max_length=100)
-   
